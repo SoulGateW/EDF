f6f0f7e0b6cf39471167795709c3d7a9f740706d
YuryXW <SoulCrash@gmail.com>
2017-04-26 12:18:40 +0000

    CTX24 joystick support added.

--- catkin_ws/src/gcv_joy_mapper/src/gcv_joy_mapper_node.py
+++ catkin_ws/src/gcv_joy_mapper/src/gcv_joy_mapper_node.py
@@ -9,6 +9,7 @@ import subprocess
 from sensor_msgs.msg import Joy
 from std_msgs.msg import Header
 from gcv_motor_driver.msg import GCVControl
+from edf.msg import Twist2DStamped
 
 TAG = "GCV Joy Mapper Node:"
 
@@ -24,26 +25,74 @@ def signal_handler(signal, frame):
 #Joystick callback
 def joy_callback(data):
     global pub
-    gain = (data.axes[5] + 1)*(1 - 0.3)/(2.0) + 0.3
-    lm = gain*data.axes[1]
-    rm = gain*data.axes[4]
+    global pub2
+    global control_mode
+    global joystick_type
 
-    h = Header()
-    h.stamp = rospy.Time.now()
+    if (control_mode == "ctx24"):
+        gain = 1.0
+    else:
+        gain = (data.axes[5] + 1)*(1 - 0.4)/(2.0) + 0.4
 
-    msg = GCVControl()
-    msg.header = h
-    msg.left_track  = lm
-    msg.right_track = rm
+    # Tank mode
+    if (control_mode == "tank"):
+        if joystick_type == "ctx24":
+            lm = gain*data.axes[1]
+            rm = gain*data.axes[2]/0.25
+            if rm>1.0:
+                rm=1.0
+        else:
+            lm = gain*data.axes[1]
+            rm = gain*data.axes[4]
 
-    pub.publish(msg)
+        h = Header()
+        h.stamp = rospy.Time.now()
+
+        msg = GCVControl()
+        msg.header = h
+        #msg.left_track  = lm
+        #msg.right_track = rm
+        msg.left_track = lm
+        msg.right_track = rm
+
+        pub.publish(msg)
+
+    # Differential drive mode
+    if (control_mode == "differential_drive"):
+        if joystick_type == "ctx24":
+            omega = data.axes[0]
+            velocity = data.axes[1]
+            #velocity = data.axes[2]/0.25
+            #if abs(velocity)>1.0:
+                #velocity = velocity/abs(velocity)
+        else:
+            omega = gain*rotation_rate*data.axes[3]
+            velocity = gain*data.axes[1]
+
+        h = Header()
+        h.stamp = rospy.Time.now()
+
+        msg = Twist2DStamped()
+        msg.header = h
+        msg.velocity = velocity
+        msg.omega = omega
+
+        pub2.publish(msg)
 
 if __name__ == '__main__':
     print TAG,"Started"
     signal.signal(signal.SIGINT, signal_handler)
+    # Starting the node
     rospy.init_node('gcv_joy_mapper_node', anonymous=True)
+    # Getting parameters
+    control_mode = rospy.get_param("~control_mode","differential_drive")
+    joystick_type = rospy.get_param("~joystick_type","default")
+    print TAG, "Control Mode  -", control_mode
+    print TAG, "Joystick Type -", joystick_type
+    # Sucscribing
     rospy.Subscriber("/joy",Joy,joy_callback)
-    pub = rospy.Publisher("/gcv_motor_driver",GCVControl, queue_size = 10)
+    pub = rospy.Publisher("/gcv_motor_driver",GCVControl, queue_size=10)
+    pub2 = rospy.Publisher("/gcv_forward_kinematics_node/cmd", Twist2DStamped, queue_size=10)
     rospy.spin()
 
     print TAG,"Terminated"
